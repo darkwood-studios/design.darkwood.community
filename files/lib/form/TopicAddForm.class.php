426d05227d9d09a082f361577fa65cbda6804a7d
TideWoW <info@faux-wow.com>
2022-09-01 17:58:49 +0200

    fixed TopicAddForm

--- files/lib/form/TopicAddForm.class.php
+++ files/lib/form/TopicAddForm.class.php
@@ -3,10 +3,13 @@
 namespace community\form;
 
 use community\data\topic\TopicAction;
+use community\data\topic\Topic;
 
 use wcf\form\AbstractFormBuilderForm;
 use wcf\util\HeaderUtil;
 use wcf\system\WCF;
+use wcf\system\request\LinkHandler;
+use wcf\system\request\IRouteController;
 use wcf\system\category\CategoryHandler;
 use wcf\system\exception\IllegalLinkException;
 use wcf\system\form\builder\field\TextFormField;
@@ -19,7 +22,7 @@ use wcf\system\form\builder\container\wysiwyg\WysiwygFormContainer;
  * Class TopicAddForm
  *
  * @package community\form
- * @author    Julian Pfeil
+ * @author    Julian Pfeil, Daniel Hass
  * @copyright    2022 Darkwood.Design
  * @license    Commercial Darkwood.Design License <https://darkwood.design/lizenz/>
  * @link    https://darkwood.design/
@@ -41,40 +44,26 @@ class TopicAddForm extends AbstractFormBuilderForm
      */
     public $objectEditLinkController = TopicEditForm::class;
 
-    /**
+   /**
      * category id
+     *
      * @var int
      */
-    public $categoryID;
+    public $categoryID = 0;
 
     /**
      * category object
+     *
      * @var Category
      */
     public $category;
 
     /**
-     * @inheritDoc
-     */
-    public function readParameters()
-    {
-        parent::readParameters();
-
-        if ($this->formAction == 'create') {
-            if (isset($_REQUEST['id'])) {
-                $this->categoryID = \intval($_REQUEST['id']);
-            }
-
-            $this->category = CategoryHandler::getInstance()->getCategory($this->categoryID);
-            
-            if ($this->category === null) {
-                throw new IllegalLinkException();
-            }
-            if ($this->category->getPermission('canViewCategory', WCF::getUser()) === 0) {
-                throw new PermissionDeniedException();
-            }
-        }
-    }
+      * topic id
+      *
+      * @var int;
+      */
+     public $topicID;
 
     /**
      * @inheritDoc
@@ -108,13 +97,63 @@ class TopicAddForm extends AbstractFormBuilderForm
     /**
      * @inheritDoc
      */
-    public function save()
+    public function readParameters()
     {
-        parent::save();
+        parent::readParameters();
 
-        /** @var Topic $topic */
-        $topic = $this->objectAction->executeAction()['returnValues'];
+        if (isset($_REQUEST['id'])) {
+            $this->categoryID = \intval($_REQUEST['id']);
+        }
 
-        HeaderUtil::redirect($topic->getLink());
+        $this->category = CategoryHandler::getInstance()->getCategory($this->categoryID);
+
+        if ($this->category === null) {
+            throw new IllegalLinkException();
+        }
+        if ($this->category->getPermission('canViewCategory', WCF::getUser()) === 0) {
+            throw new PermissionDeniedException();
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function setFormAction()
+    {
+        $parameters = [];
+        $parameters['id'] = $this->categoryID;
+        if ($this->formObject !== null) {
+            if ($this->formObject instanceof IRouteController) {
+                $parameters['object'] = $this->formObject;
+                $this->topicID = $this->formObject->topicID;
+            } else {
+                $object = $this->formObject;
+                $this->topicID = $object->topicID;
+                $parameters['topicID'] = $object->topicID;
+            }
+        }
+
+        $this->form->action(LinkHandler::getInstance()->getControllerLink(static::class, $parameters));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function saved()
+    {
+        parent::saved();
+
+        if ($this->topicID == 0) {
+            $this->topicID = $this->objectAction->getReturnValues()['returnValues']->topicID;
+        }
+
+        $topic = new Topic($this->topicID);
+        HeaderUtil::redirect(
+            LinkHandler::getInstance()->getLink('Topic', [
+                'application' => 'community',
+                'object' => $topic
+            ])
+        );
+        exit;
     }
 }
