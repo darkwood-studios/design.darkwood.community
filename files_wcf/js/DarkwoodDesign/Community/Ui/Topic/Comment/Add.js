60ac7fa9b01265a05388ca99be1bf39e5845edca
Darkwood.Design <info@darkwood.design>
2022-09-28 18:55:59 +0200

    Optionen hinzugefügt

--- files_wcf/js/DarkwoodDesign/Community/Ui/Topic/Comment/Add.js
+++ files_wcf/js/DarkwoodDesign/Community/Ui/Topic/Comment/Add.js
@@ -10,11 +10,11 @@
  */
 define(["require", "exports", "tslib", "WoltLabSuite/Core/Dom/Change/Listener", "WoltLabSuite/Core/Dom/Util", "WoltLabSuite/Core/Language", "WoltLabSuite/Core/Ui/Notification", "WoltLabSuite/Core/Ui/Comment/Add"], function (require, exports, tslib_1, Listener_1, Util_1, Language, UiNotification, Add_1) {
     "use strict";
-    Listener_1 = tslib_1.__importDefault(Listener_1);
-    Util_1 = tslib_1.__importDefault(Util_1);
-    Language = tslib_1.__importStar(Language);
-    UiNotification = tslib_1.__importStar(UiNotification);
-    Add_1 = tslib_1.__importDefault(Add_1);
+    Listener_1 = (0, tslib_1.__importDefault)(Listener_1);
+    Util_1 = (0, tslib_1.__importDefault)(Util_1);
+    Language = (0, tslib_1.__importStar)(Language);
+    UiNotification = (0, tslib_1.__importStar)(UiNotification);
+    Add_1 = (0, tslib_1.__importDefault)(Add_1);
     class UiTopicCommentAdd extends Add_1.default {
         /**
          * Inserts the rendered message.
